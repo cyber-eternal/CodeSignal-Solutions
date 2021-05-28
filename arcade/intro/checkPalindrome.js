f65bb5242d33172cc93706ffd6c1ceae4d71852a
Edgar Sargsyan <92edgarsargsyan@gmail.com>
2021-05-28 23:28:54 +0400

    feat: add checkPalindrome.js

---
+++ arcade/intro/checkPalindrome.js
@@ -0,0 +1,19 @@
+/*
+Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true.
+*/
+
+
+
+function checkPalindrome(inputString) {
+  return inputString == inputString.split('').reverse().join('');
+}
+
