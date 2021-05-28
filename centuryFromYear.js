038b201f1cb80970c54ec9c53863b2794fb49281
Edgar Sargsyan <92edgarsargsyan@gmail.com>
2021-05-28 23:24:41 +0400

    Create centuryFromYear.js

---
+++ centuryFromYear.js
@@ -0,0 +1,16 @@
+/*
+
+Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+
+Example
+
+For year = 1905, the output should be
+centuryFromYear(year) = 20;
+For year = 1700, the output should be
+centuryFromYear(year) = 17.
+
+*/
+
+function centuryFromYear(year) {
+    return Math.ceil(year/100)
+}
