29296671908ea0f1322a91306213d53d8f1ab59e
Edgar Sargsyan <92edgarsargsyan@gmail.com>
2021-07-11 21:52:41 +0400

    Create areSimilar.js

---
+++ arcade/intro/areSimilar.js
@@ -0,0 +1,5 @@
+function areSimilar(a, b) {
+    const ad = a.filter((v,i)=>v!=b[i])
+    const bd = b.filter((v,i)=>v!=a[i])
+    return ad.length == 0 || (ad.length == 2 && ad.join('') == bd.reverse().join(''))
+}
