3143a091413042ef416f3df56765267e88cf08fb
Edgar Sargsyan <92edgarsargsyan@gmail.com>
2021-09-02 22:03:47 +0400

    Create variableName.js

---
+++ arcade/intro/variableName.js
@@ -0,0 +1,34 @@
+/*
+
+Correct variable names consist only of English letters, digits and underscores and they can't start with a digit.
+
+Check if the given string is a correct variable name.
+
+Example
+
+For name = "var_1__Int", the output should be
+variableName(name) = true;
+For name = "qq-q", the output should be
+variableName(name) = false;
+For name = "2w2", the output should be
+variableName(name) = false.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] string name
+
+Guaranteed constraints:
+1 ≤ name.length ≤ 10.
+
+[output] boolean
+
+true if name is a correct variable name, false otherwise.
+
+*/
+
+
+function variableName(name) {
+  let test = /^[a-zA-Z_]\w*/.exec(name);
+    return test != null && test[0] === name;
+}
