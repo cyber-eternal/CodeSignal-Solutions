b0bc5fefa1a721f264b1c5180e0c07ebc0018327
Edgar Sargsyan <92edgarsargsyan@gmail.com>
2021-09-02 22:08:30 +0400

    Create depositProfit.js

---
+++ arcade/intro/depositProfit.js
@@ -0,0 +1,25 @@
+/*
+
+You have deposited a specific amount of money into your bank account. Each year your balance increases at the same growth rate. With the assumption that you don't make any additional deposits, find out how long it would take for your balance to pass a specific threshold.
+
+Example
+
+For deposit = 100, rate = 20, and threshold = 170, the output should be
+depositProfit(deposit, rate, threshold) = 3.
+
+Each year the amount of money in your account increases by 20%. So throughout the years, your balance would be:
+
+year 0: 100;
+year 1: 120;
+year 2: 144;
+year 3: 172.8.
+Thus, it will take 3 years for your balance to pass the threshold, so the answer is 3.
+
+*/
+
+
+function depositProfit(deposit, rate, threshold) {
+    for(let y = 1; ; y++) {
+        if((deposit += rate / 100 * deposit) >= threshold) return y;
+    }
+}
