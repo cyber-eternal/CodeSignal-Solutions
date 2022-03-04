8c270f9903f0b85b1efe5d1acacf1a74d145f973
Edgar Sargsyan <92edgarsargsyan@gmail.com>
2022-03-04 23:07:42 +0400

    add bishopAndPawn.js

---
+++ arcade/intro/bishopAndPawn.js
@@ -0,0 +1,27 @@
+/*
+
+Given the positions of a white bishop and a black pawn on the standard chess board, determine whether the bishop can capture the pawn in one move.
+
+The bishop has no restrictions in distance for each move, but is limited to diagonal movement. Check out the example below to see how it can move:
+
+Example
+
+For bishop = "a1" and pawn = "c3", the output should be
+solution(bishop, pawn) = true.
+
+
+
+For bishop = "h1" and pawn = "h3", the output should be
+solution(bishop, pawn) = false.
+
+
+
+*/
+
+
+function solution(bishop, pawn) {
+	return (
+		Math.abs(bishop.charCodeAt(0) - pawn.charCodeAt(0)) ==
+		Math.abs(bishop.charCodeAt(1) - pawn.charCodeAt(1))
+	);
+}
