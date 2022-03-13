7fdda3aa0854975b3e94dff311b914087c0320b7
Edgar Sargsyan <92edgarsargsyan@gmail.com>
2022-03-13 13:35:47 +0400

    add lineEncoding.js

---
+++ arcade/intro/lineEncoding.js
@@ -0,0 +1,20 @@
+/*
+
+Given a string, return its encoding defined as follows:
+
+First, the string is divided into the least possible number of disjoint substrings consisting of identical characters
+for example, "aabbbc" is divided into ["aa", "bbb", "c"]
+Next, each substring with length greater than one is replaced with a concatenation of its length and the repeating character
+for example, substring "bbb" is replaced by "3b"
+Finally, all the new strings are concatenated together in the same order and a new string is returned.
+Example
+
+For s = "aabbbc", the output should be
+solution(s) = "2a3bc"
+
+*/
+
+
+function solution(s) {
+	return s.replace(/(.)\1*/g, (e, i) => (i == e ? i : e.length + i));
+}
