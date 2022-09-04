3e9fb62c98c8217bc3ae8e7957efb6375efc68a5
Edgar Sargsyan <92edgarsargsyan@gmail.com>
2022-09-04 08:25:32 +0800

    Create deleteDigit.js

---
+++ arcade/intro/deleteDigit.js
@@ -0,0 +1,21 @@
+/*
+
+Given some integer, find the maximal number you can obtain by deleting exactly one digit of the given number.
+
+Example
+
+For n = 152, the output should be
+solution(n) = 52;
+For n = 1001, the output should be
+solution(n) = 101.
+
+*/
+
+function solution(n) {
+	const m = 0;
+	for (let d = 1; d <= n; d *= 10) {
+		const t = (n % d) + Math.floor(Math.floor(n / d) / 10) * d;
+		m = Math.max(m, t);
+	}
+	return m;
+}
