6bb34fa27670d3e9bd2aff499130b30625eed5e7
Edgar Sargsyan <92edgarsargsyan@gmail.com>
2022-09-18 14:37:56 +0800

    Create ValidTime.js

---
+++ arcade/intro/ValidTime.js
@@ -0,0 +1,34 @@
+/*
+
+Check if the given string is a correct time representation of the 24-hour clock.
+
+Example
+
+For time = "13:58", the output should be
+solution(time) = true;
+For time = "25:51", the output should be
+solution(time) = false;
+For time = "02:76", the output should be
+solution(time) = false.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] string time
+
+A string representing time in HH:MM format. It is guaranteed that the first two characters, as well as the last two characters, are digits.
+
+[output] boolean
+
+true if the given representation is correct, false otherwise.
+
+*/
+
+
+function solution(time) {
+  const [hours, minutes] = time.split(':');
+
+  if(hours > 23 || minutes > 59) return false;
+
+  return true
+}
